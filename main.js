475836a970221111a34529a723ee6b6329975ef6
Chitranshi1~ <chitranshigourana@gmail.com>
2024-02-07 14:26:40 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const fetchData = async () => {
+  const userData = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await userData.json();
+
+
+  // console.log(userData).
+  users.forEach(data => {
+    
+    let tbody = document.createElement('tbody');
+
+    tbody.innerHTML += `<tr>
+      <th>${data.id}</th>
+      <td>${data.name}</td>
+      <td>${data.email}</td>
+      <td>${data.address.city}</td>
+    </tr>`
+
+   let table = document.querySelector('table');
+   table.appendChild(tbody)
+  });
+}
+
+let button = document.querySelector('button');
+
+button.addEventListener('click', fetchData);
